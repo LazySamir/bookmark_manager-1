389c5861731a2a16de31fc123cf2ae374cd9c146
davidlawes <davidlawes@hotmail.co.uk>
2018-08-29 18:38:28 +0100

    Refactored model app.rb

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,24 +3,23 @@ require 'pg'
 class Bookmark
 
   def self.all
-    self.determine_database
-    connection = PG.connect(dbname: @db)
-    result = connection.exec("SELECT * FROM bookmarks;")
+    self.create_connection_to_database
+    result = @connection.exec("SELECT * FROM bookmarks;")
     result.map { |bookmark| bookmark['url'] }
   end
 
   def self.create(url)
-    self.determine_database
-    connection = PG.connect(dbname: @db)
-    connection.exec("INSERT INTO bookmarks(url) VALUES (\'#{url}\');")
+    self.create_connection_to_database
+    @connection.exec("INSERT INTO bookmarks(url) VALUES ('#{url}');")
   end
 
-  def self.determine_database
+  def self.create_connection_to_database
     if ENV['RACK_ENV'] == 'test'
-      @db = 'bookmark_manager_test'
+      db = 'bookmark_manager_test'
     else
-      @db = 'bookmark_manager'
+      db = 'bookmark_manager'
     end
+    @connection = PG.connect(dbname: db)
   end
 
 
